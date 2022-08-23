1db820981635181d5b87e207b3fe2f7158ed5aa0
omcode <o.d.pouso@gmail.com>
2022-08-22 21:34:42 -0300

    2da Entrega FitShop Delgado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import MiPrimerComponente from './components/MiPrimerComponente';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
+        <MiPrimerComponente/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/MiPrimerComponente.js
@@ -0,0 +1,9 @@
+const MiPrimerComponente = () => {
+    return(
+        <div style={{width:'120px'}}>
+            <h2>Hola soy un componente</h2>
+        </div>
+    );
+};
+
+export default MiPrimerComponente;
