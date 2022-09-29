dd7933bd94d4f068a45300cf7f59993e536103fc
omcode <o.d.pouso@gmail.com>
2022-09-28 22:47:01 -0300

    FitShop Omar Delgado 28sept

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { CartContext } from "../Context/CartContext";
 import {useContext} from 'react';
 import {Link} from 'react-router-dom';
+import {db} from '../../utils/firebase';
+import {collection, addDoc, doc, updateDoc} from 'firebase/firestore';
 
 function CartContainer() {
     const {listaDeCompras, removeItem, clear, getPrecioTotal}= useContext(CartContext);
+    const [idOrden, setIdOrden] = useState('');
+    
+    
+    const enviarOrden = (event) =>{
+        event.preventDefault();
+        const orden ={
+            buyer:{
+            name: event.target[0],
+            telefono: event.target[1],
+            mail: event.target[2]
+            },
+            items:listaDeCompras,
+            total: getPrecioTotal()
+        };
+        console.log('orden enviada', orden)
+        const querryRef = collection(db,'ordenes');
+        addDoc(querryRef, orden).then(response=>{
+            console.log('response', response)
+            setIdOrden(response.id);
+        });
+    };
+
+    const updateOrden = () =>{
+        const querryRef = doc(db, 'ordenes', 'id q no me sale');
+        updateDoc(querryRef, )
+    };
+
+
     return(
         <div>
+            <button onClick={updateOrden}>Actualizar orden</button>
+            {idOrden && <p>se creo su orden {idOrden}</p>}
             {
             listaDeCompras.length > 0 ?
             <div>
@@ -21,6 +53,15 @@ function CartContainer() {
                 ))}
                 <h3>Precio Total: {getPrecioTotal()}</h3>
                 <button onClick={()=>clear()}>Despejar</button>
+                <form onSubmit={enviarOrden}>
+                    <label>Nombre:</label>
+                    <input type='text'></input>
+                    <label>Telefono:</label>
+                    <input type='text'></input>
+                    <label>Mail:</label>
+                    <input type='email'></input>
+                    <button type='submit'>Enviar orden</button>
+                </form>
             </div>
             :
             <div>   
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,20 @@
-import {initializeApp} from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 
-const app = initializeApp(firebaseConfig);
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxkxf3QpIQebljENyQXUqtBOCVwEyyFZo",
+  authDomain: "fitshop-1b7e4.firebaseapp.com",
+  projectId: "fitshop-1b7e4",
+  storageBucket: "fitshop-1b7e4.appspot.com",
+  messagingSenderId: "382617976427",
+  appId: "1:382617976427:web:5a17f4e1e9b336a863fb2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
